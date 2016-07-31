374d6adbc41a3669b50476b5e99ac23f45a686f7
Marcos Barbero <marcos.hgb@gmail.com>
2016-07-31 16:22:20 -0300

    Adjustment for spring boot 1.3.5.RELEASE

--- auth-service/src/main/java/com/marcosbarbero/authserver/config/oauth2/LogoutSuccessHandler.java
+++ auth-service/src/main/java/com/marcosbarbero/authserver/config/oauth2/LogoutSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.marcosbarbero.authserver.config.oauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
--- auth-service/src/main/java/com/marcosbarbero/authserver/config/oauth2/WebSecurityConfig.java
+++ auth-service/src/main/java/com/marcosbarbero/authserver/config/oauth2/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.marcosbarbero.authserver.config.oauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,15 +26,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder);
+    }
+
+    @Bean
+    @Override
+    @Qualifier("authenticationManagerBean")
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(this.userDetailsService)
                 .passwordEncoder(this.passwordEncoder);
-    }
-
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder);
     }
 
 }
