bbaac02a7021c6d5acf728d5304ab78fe0122614
Marcos Barbero <marcos.hgb@gmail.com>
2016-08-03 03:35:04 -0300

    Add configserver

--- discovery-service/src/test/java/com/marcosbarbero/DiscoveryApplicationTest.java
+++ discovery-service/src/test/java/com/marcosbarbero/DiscoveryApplicationTest.java
@@ -3,11 +3,8 @@ package com.marcosbarbero;
 import com.marcosbarbero.discovery.DiscoveryApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * @author Marcos Barbero
